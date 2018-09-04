697021b7134e7fbe7867ae849428f93ebccf9871
Lev V. Babchenko <gour@mail.ru>
2018-09-04 18:56:14 +0500

    stub for solution 3 added (NOT PASSED!)

--- ControlTask.cs
+++ ControlTask.cs
@@ -2,11 +2,17 @@ using System;
 
 namespace func_rocket
 {
-	public class ControlTask
-	{
-		public static Turn ControlRocket(Rocket rocket, Vector target)
-		{
-			return Turn.None;
-		}
-	}
+    public class ControlTask
+    {
+        public static Turn ControlRocket(Rocket rocket, Vector target)
+        { // FIXME!
+            Vector dt = rocket.Location - target;
+            Vector f = dt - rocket.Velocity;
+            double desired = f.Angle;
+            double rotor = (desired - rocket.Direction) % (Math.PI * 2) - Math.PI;
+            return (rotor < 0) ? Turn.Left
+                : (0 < rotor) ? Turn.Right
+                : Turn.None;
+        }
+    }
 }
