24f0475e0632c7ff34d3e5b5a1b15ea1b639c1ab
Lev V. Babchenko <gour@mail.ru>
2018-09-04 18:52:25 +0500

    initial commit

---
+++ ForcesTask.cs
@@ -0,0 +1,33 @@
+﻿using System.Drawing;
+using System.Linq;
+
+namespace func_rocket
+{
+	public class ForcesTask
+	{
+		/// <summary>
+		/// Создает делегат, возвращающий по ракете вектор силы тяги двигателей этой ракеты.
+		/// Сила тяги направлена вдоль ракеты и равна по модулю forceValue.
+		/// </summary>
+		public static RocketForce GetThrustForce(double forceValue)
+		{
+			return r => Vector.Zero;
+		}
+
+		/// <summary>
+		/// Преобразует делегат силы гравитации, в делегат силы, действующей на ракету
+		/// </summary>
+		public static RocketForce ConvertGravityToForce(Gravity gravity, Size spaceSize)
+		{
+			return r => Vector.Zero;
+		}
+
+		/// <summary>
+		/// Суммирует все переданные силы, действующие на ракету, и возвращает суммарную силу.
+		/// </summary>
+		public static RocketForce Sum(params RocketForce[] forces)
+		{
+			return forces[0];
+		}
+	}
+}
